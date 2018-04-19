deb631b3056c15cafc72950bece7f0db65b9c0bb
Riys Hassam <riys@telus.net>
2018-04-19 15:03:26 -0700

    Main

---
+++ ForgeWorkspace/src/main/java/com/hassam/classcraft/util/Reference.java
@@ -0,0 +1,10 @@
+package com.hassam.classcraft.util;
+
+public class Reference {
+	public static final String MODID = "cc";
+	public static final String NAME = "ClassCraft";
+	public static final String VERSION = "0.0.1ALPHA";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT = "com.hassam.classcraft.proxy.ClientProxy";
+	public static final String COMMON = "com.hassam.classcraft.proxy.CommonProxy";
+}
